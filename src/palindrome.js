3752f6cbdd8959a2637a19cbfb0905a5526e44e2
senthilp <senthil.padmanabhan@yahoo.com>
2014-09-30 20:59:33 -0700

    First cut

---
+++ src/palindrome.js
@@ -0,0 +1,19 @@
+var isPallindrome = function(source) {
+	if(!source) {
+		return false;
+	}
+	// Normalize source
+	source = source.toLowerCase().replace(/\W+/g, '');
+	var reverseStr = [],
+		i;
+	for(i = source.length - 1; i >= 0; i--) {
+		reverseStr.push(source[i]);
+	}
+	reverseStr = reverseStr.join('');
+	console.log('Reversed String: ' + reverseStr);
+	return source === reverseStr;
+};
+
+console.log('Pallindrome Check - ' + isPallindrome('A man, a plan, a canal: Panama'));
+
+module.exports = isPallindrome;
